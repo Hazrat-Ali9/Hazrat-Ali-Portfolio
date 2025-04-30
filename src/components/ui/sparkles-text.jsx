865651c8c2e61817d50cb55042d447c2d435da69
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-04-30 10:02:44 +0600

    Sparkles Text  🐻‍❄️

--- src/components/ui/sparkles-text.jsx
+++ src/components/ui/sparkles-text.jsx
@@ -1,7 +1,7 @@
 "use client";;
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-
+// Sparkles Text 
 import { cn } from "@/lib/utils";
 
 const SparklesText = ({
