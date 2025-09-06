8c4fe1d7f9306496b9dc32e72b827e9abceaf6b6
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-09-06 20:15:39 +0600

    apps jsx  🏝

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Header from "./pages/Header/Header";
 import Hero from "./pages/Hero/Hero";
 import Skills from "./pages/Skills/Skills";
 import Education from "./pages/Education/Education";
-// apps jsx
+// Apps jsx
 import { Route, Routes } from "react-router-dom";
 
 export default function App() {
